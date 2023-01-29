2869debc76bcb1ee88378d42934391f1457f02bc
Karthikeyan M <113222543+Karthikeyan2752@users.noreply.github.com>
2023-01-29 14:27:35 +0530

    Update and rename ChatBot.java to ChatBotOld.java

--- ChatBotOld.java
+++ ChatBotOld.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class ChatBot {
+public class ChatBotOld {
 
 
 	static List<Subscriber> subs = new ArrayList<>();
